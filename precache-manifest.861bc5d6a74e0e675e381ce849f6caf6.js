fcf62d96033f0e55e8bf96f14246a844a4570428
iamskab <iamsourav470@gmail.com>
2020-07-04 10:46:27 +0530

    Updates

---
+++ precache-manifest.861bc5d6a74e0e675e381ce849f6caf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c720e2a17ce987d526f25d13b4f6977b",
+    "url": "/jukeboxYouTube/index.html"
+  },
+  {
+    "revision": "19805caf173afc04e571",
+    "url": "/jukeboxYouTube/static/css/2.6375f8dd.chunk.css"
+  },
+  {
+    "revision": "6b141f0a50c58895d249",
+    "url": "/jukeboxYouTube/static/css/main.f9d41231.chunk.css"
+  },
+  {
+    "revision": "19805caf173afc04e571",
+    "url": "/jukeboxYouTube/static/js/2.4b2096d8.chunk.js"
+  },
+  {
+    "revision": "780018507830bdb45449f873b52d1575",
+    "url": "/jukeboxYouTube/static/js/2.4b2096d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b141f0a50c58895d249",
+    "url": "/jukeboxYouTube/static/js/main.cda79453.chunk.js"
+  },
+  {
+    "revision": "10065b766dd2fdf6eb8e",
+    "url": "/jukeboxYouTube/static/js/runtime-main.f0acdfca.js"
+  }
+]);
